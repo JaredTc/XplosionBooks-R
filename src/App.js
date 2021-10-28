7e7326c2a4bf19e71d45b7d5a316aa4e961c5560
marcela <marcela.2209@hotmail.com>
2021-10-27 20:45:48 -0500

    se adiciono archivo registro

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import './App.css';
 // import {Header} from '../src/layouts/Header'
 import {Footer} from  '../src/layouts/footer'
- import {HomePage} from '../src/layouts/Homepague'
+import {HomePage} from '../src/layouts/Homepague'
 import {DestadoPage} from './layouts/destacado2'
 import { BrowserRouter, Switch, Route,} from "react-router-dom";
 import { Category } from './layouts/categorias';
